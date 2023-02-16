5c4d4701d8e7fe35160178e299b405b2444ff59a
GitHub Action <action@github.com>
2023-02-16 09:35:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/shipanliu/pluginhelloworld/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/shipanliu/pluginhelloworld/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.shipanliu.pluginhelloworld.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shipanliu.pluginhelloworld.MyBundle
 
 class MyApplicationService {
 
